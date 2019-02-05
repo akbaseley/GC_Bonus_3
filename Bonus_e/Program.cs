25d7ce2e47296b292380750974eccc1ff9d2671b
Anna B <akbaseley@gmail.com>
2019-02-05 10:25:08 -0500

    Added the option to continue playing and cleaned up the if-statements into a method

--- Bonus_e/Program.cs
+++ Bonus_e/Program.cs
@@ -10,45 +10,26 @@ namespace Bonus_e
     {
         static void Main(string[] args)
         {
+            bool userContinue = true;
+
             Console.WriteLine("Welcome to the Grand Circus Number Guessing Game!");
-
-            int computerNumber = GetNumber(0, 100);
-            int difference = SubtractNumbers(GetNumber("Please enter a number between 1 and 100.", 0, 100), computerNumber);
-            
-            int count = 1;
-            string message = "";
-            while (difference != 0)
+            while (userContinue)
             {
-                //compare statements here 
-                if(difference > 0)
-                {
-                    //userNumber is higher than computer number
-                    if(difference > 10)
-                    {
-                        message = "Number's way too high, ya dummy!";
-                    }
-                    else
-                    {
-                        message = "Number's just a bit too high!";
-                    }
-                }
-                else
-                {
-                    //computerNumber is higher than userNumber
+                int computerNumber = GetNumber(0, 100);
+                int difference = SubtractNumbers(GetNumber("Please enter a number between 1 and 100.", 0, 100), computerNumber);
 
-                    if (difference < -10)
-                    {
-                        message = "Number's waaayyy too low, dumb-dumb!";
-                    }
-                    else
-                    {
-                        message = "Number's too low, bud!";
-                    }
+                int count = 1;
+                string message = "";
+                while (difference != 0)
+                {
+                    message = EvaluateNumber(difference);
+                    count++;
+                    difference = SubtractNumbers(GetNumber(message, 0, 100), computerNumber);
                 }
-                count++;
-                difference = SubtractNumbers(GetNumber(message, 0, 100), computerNumber);
+                ReturnFinalPhrase(count);
+                userContinue = ContinueProgram("Would you like to play again? y/n");
             }
-            ReturnFinalPhrase(count);
+            Console.WriteLine("Thank you for playing! Bye!");
         }
 
         public static int GetNumber(int min, int max)
@@ -57,7 +38,7 @@ namespace Bonus_e
             return r.Next(min, max);
         }
         public static int GetNumber(string message, int min, int max)
-        { 
+        {
             int userNumber = -1;
             while (userNumber < min || userNumber > max)
             {
@@ -72,7 +53,7 @@ namespace Bonus_e
         }
         public static void ReturnFinalPhrase(int number)
         {
-            if(number > 1)
+            if (number > 1)
             {
                 Console.WriteLine($"You guessed the number in {number} tries!");
             }
@@ -81,6 +62,54 @@ namespace Bonus_e
                 Console.WriteLine($"Great job you guessed the number on your first try!");
             }
         }
+        public static string EvaluateNumber(int number)
+        {
+            string message;
+            //compare statements here 
+            if (number > 0)
+            {
+                //userNumber is higher than computer number
+                if (number > 10)
+                {
+                    message = "Number's way too high, ya dummy!";
+                }
+                else
+                {
+                    message = "Number's just a bit too high!";
+                }
+            }
+            else
+            {
+                //computerNumber is higher than userNumber
 
+                if (number < -10)
+                {
+                    message = "Number's waaayyy too low, dumb-dumb!";
+                }
+                else
+                {
+                    message = "Number's too low, bud!";
+                }
+            }
+            return message;
+        }
+        public static bool ContinueProgram(string message)
+        {
+            Console.WriteLine(message);
+            string userInput = Console.ReadLine();
+
+            if(userInput == "y")
+            {
+                return true;
+            }
+            else if(userInput == "n")
+            {
+                return false;
+            }
+            else
+            {
+               return ContinueProgram("Not a valid response! " + message);
+            }
+        }
     }
 }
